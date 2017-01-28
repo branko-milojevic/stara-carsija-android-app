e53fe0ed07cf680e1ce08e2e3cabc7e5163b645e
branko-milojevic <vitkovac.vitkovac@gmail.com>
2017-01-28 22:50:34 +0100

    Uveden prikaz toasta-i poziv get metoda, potrebno odvojiti activity iz kog se poziva login

--- app/src/main/java/com/example/vitko/ordermeal/MainActivity.java
+++ app/src/main/java/com/example/vitko/ordermeal/MainActivity.java
@@ -1,9 +1,24 @@
 package com.example.vitko.ordermeal;
 
+import android.app.Dialog;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.example.vitko.ordermeal.R.layout.activity_main;
 
@@ -13,5 +28,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_main);
+
+        Dialog loginDialog = new Dialog(this);
+        loginDialog.setContentView(R.layout.activity_main);
+        Button loginButton = (Button) loginDialog.findViewById(R.id.button);
+
     }
+    public void displayLoginMessage(View v) {
+        Toast toast = Toast.makeText(getApplication(), "hello, i will make you log in", Toast.LENGTH_LONG);
+        toast.show();
+        login();
+    }
+
+    private void login(){
+        try {
+            URL loginUrl = new URL("https://goo.gl/JHpqCD");
+
+            HttpURLConnection conn = (HttpURLConnection) loginUrl.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+
+
+            int responseCode=conn.getResponseCode();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
